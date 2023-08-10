02f5c8617ce069b0c94cc6e137f789e20971b036
Anoop Chaudhary <anooprahulchaudhary@gmail.com>
2023-08-10 08:52:54 +0530

    Initial Changes
    Committer: Anoop Chaudhary <anooprahulchaudhary@gmail.com>

---
+++ src/main/java/com/example/practicemongo/PracticeMongoApplication.java
@@ -0,0 +1,13 @@
+package com.example.practicemongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeMongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/practicemongo/PracticeMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practicemongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
